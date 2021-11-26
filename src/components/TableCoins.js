58a4580f93dfba56772de7a0faa885715b8d4068
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2021-11-25 23:17:08 -0500

    useMemo in filter
    added the useMemo hook to the search filter to optimize the platform by IP, it saves calculations or logic previously carried out in an IP to be more efficient and optimize the product in future uses, this is called memoization

--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import RowCoin from './RowCoin';
 
 const tableTitles = ["#", "Name", "Price (USD)", "24h %", "Market Cap", "Circulating Supply"];
 
 const TableCoins = ({coins, search}) => {
 
-    const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toString().toLowerCase()) | coin.symbol.toString().toLowerCase().includes(search.toString().toLowerCase()));
+    const filteredCoins = useMemo(() => {
+        return coins.filter((coin) => {
+            return coin.name.toLowerCase().includes(search.toString().toLowerCase()) | coin.symbol.toString().toLowerCase().includes(search.toString().toLowerCase());
+        })
+    }, [coins, search]);
 
     return (
         <table className="table table-dark mt-4 table-hover">
